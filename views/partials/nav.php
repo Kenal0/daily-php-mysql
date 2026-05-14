1fdb9db977763d37bc7ec2f8efe1bf7287a5a61e
Kenal0 <kenal.karpenko.mikhail@gmail.com>
2026-05-14 16:37:52 +0300

    fix avatar image loading

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -3,7 +3,7 @@
         <div class="flex h-16 items-center justify-between">
             <div class="flex items-center">
                 <div class="flex-shrink-0">
-                    <a href="/"> <img class="h-8 w-8" src="https://taigilwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company"> </a>
+                    <a href="/"> <img class="h-8 w-8" src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company"> </a>
                 </div>
                 <div class="hidden md:block">
                     <div class="ml-10 flex items-baseline space-x-4">
@@ -33,7 +33,7 @@
                             <?php if ($_SESSION['user'] ?? false) : ?>
                             <button type="button" class="flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
                                 <span class="sr-only">Open user menu</span>
-                                <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+                                <img class="h-8 w-8 rounded-full" src="/images/profile1.avif" alt="Profile picture">
                                 </button>
                             <?php else : ?>
                                     <a href="/register" class="<?= urlIs('/register') ? 'bg-gray-900 text-white' : 'text-gray-300' ?> hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Register</a>
