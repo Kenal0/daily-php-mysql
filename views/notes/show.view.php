89fc7c9bb33c51c7c70a20c94306fe41636b40db
Kenal0 <kenal.karpenko.mikhail@gmail.com>
2026-05-14 15:52:43 +0300

    added delete button on two pages

--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -9,9 +9,21 @@
         </p>
         <p><?= htmlspecialchars($note['body']) ?></p>
 
-        <footer class="mt-6">
+        <div class="flex gap-6 ">
+        <form method="POST" action="/note" class="mt-6">
+            <input type="hidden" name="_method" value="DELETE">
+            <input type="hidden" name="id" value="<?= $note['id'] ?>">
+                <button type="submit"
+                        onclick="return confirm('DELETE NOTE?')"
+            class="inline-flex justify-center rounded-md border border-transparent bg-gray-500 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                Delete
+            </button>
+        </form>
+        <button class="mt-6">
             <a href="/note/edit?id=<?= $note['id'] ?>" class="inline-flex justify-center rounded-md border border-transparent bg-gray-500 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Edit</a>
-        </footer>
+        </button>
+        </div>
+
 
 
 
